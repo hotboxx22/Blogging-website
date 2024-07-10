3efabdbc8ef3c03eb3049096b531140918017204
hotboxx22 <115435037+hotboxx22@users.noreply.github.com>
2024-07-10 15:12:30 +0530

    script.js

--- script.js
+++ script.js
@@ -88,3 +88,10 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
+  function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
